1a3da106e21fae531ce244899c78312201b5ed61
alvisevenezia <wantoine@outlook.fr>
2021-06-16 17:04:17 +0200

    Ajout code arduino

---
+++ Capteur_Temp_rature.ino
@@ -0,0 +1,137 @@
+#include <OneWire.h>
+#include <SoftwareSerial.h>
+ 
+/* Broche du bus 1-Wire */
+const byte BROCHE_ONEWIRE = 7;
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+/* Code de retour de la fonction getTemperature() */
+enum DS18B20_RCODES {
+  READ_OK,  // Lecture ok
+  NO_SENSOR_FOUND,  // Pas de capteur 
+  INVALID_ADDRESS,  // Adresse reçue invalide
+  INVALID_SENSOR  // Capteur invalide (pas un DS18B20)
+};
+
+SoftwareSerial hc05(2 ,3);
+
+
+/* Création de l'objet OneWire pour manipuler le bus 1-Wire */
+OneWire ds(BROCHE_ONEWIRE);
+ 
+ 
+/**
+ * Fonction de lecture de la température via un capteur DS18B20.
+ */
+byte getTemperature(float *temperature, byte reset_search) {
+  byte data[9], addr[8];
+  // data[] : Données lues depuis le scratchpad
+  // addr[] : Adresse du module 1-Wire détecté
+  
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  if (reset_search) {
+    ds.reset_search();
+  }
+ 
+  /* Recherche le prochain capteur 1-Wire disponible */
+  if (!ds.search(addr)) {
+    // Pas de capteur
+    return NO_SENSOR_FOUND;
+  }
+  
+  /* Vérifie que l'adresse a été correctement reçue */
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    // Adresse invalide
+    return INVALID_ADDRESS;
+  }
+ 
+  /* Vérifie qu'il s'agit bien d'un DS18B20 */
+  if (addr[0] != 0x28) {
+    // Mauvais type de capteur
+    return INVALID_SENSOR;
+  }
+ 
+  /* Reset le bus 1-Wire et sélectionne le capteur */
+  ds.reset();
+  ds.select(addr);
+  
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds.write(0x44, 1);
+  delay(800);
+  
+  /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
+  ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);
+ 
+ /* Lecture du scratchpad */
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+   
+  /* Calcul de la température en degré Celsius */
+  *temperature = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
+  // Pas d'erreur
+  return READ_OK;
+}
+ 
+ 
+/** Fonction setup() **/
+void setup() {
+  
+  /* Initialisation du capteur */
+  hc05.begin(9600);
+}
+
+void loop() {
+    recvWithStartEndMarkers();
+
+    if(newData == true){
+
+      if(strcmp(receivedChars,"wake up") == 0){
+
+        float temperature;
+        getTemperature(&temperature,true);
+        hc05.println(temperature);
+        newData = false;
+        
+      }
+      
+    }
+    
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    while (hc05.available() > 0 && newData == false) {
+        rc = hc05.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
